ce18cd47a3e930a95dcf3afce741d32713a513cf
zemin <731583657@qq.com>
2019-06-26 10:27:36 +0800

    删除原来项目

--- view/src/routes/Config.js
+++
@@ -1,9 +0,0 @@
-global.constants = {
-  // onlineWeb: '123.207.39.209:8090',
-  // onlineWeb: '10.88.89.148:8090',
-  // onlineWeb: '120.79.208.157:8090',
-  // onlineWeb: '123.207.88.210:8090',
-  onlineWeb: 'http://10.88.89.101:8880',
-  Authorization:
-    'Bearer eyJhbGciOiJIUzI1NiIsInppcCI6IkRFRiJ9.eNqqVspMLFGyMjQ1NTQ2MjK2tNBRSixNUbJSKk9NUtJRSq0ogEmaGIIkS4tTi_wSc1OBKopLC1KLElNyM_OUagEAAAD__w.TRH7E2NyAL2HhXXIbTUwJOEHtzd3NxyWY2WMlnKt-2I',
-};
